ceb108bac0d38df18da4ace7602dca85530585e1
Guillaume S <gu.dev@laposte.net>
2025-10-13 10:18:20 +0200

    ajout squelette TD2

---
+++ TD2/skeleton_ex1/utils.c
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+bool is_sorted_nondecreasing(int *arr, int n)
+{
+    // return true if array is sorted and false otherwise
+}
+
+int min_int(int a, int b)
+{
+    // return smallest value between a and b
+}
---
+++ TD2/skeleton_ex2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "sort.h"
+#include "utils.h"
+
+int main(void)
+{
+    // implement main here
+    // create array example using malloc
+    // use sorting algorithms and compare runtime
+    return 0;
+}
