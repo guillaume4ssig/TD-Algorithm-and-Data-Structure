164d09bdc2487b9ad9352b8ccd31db87421203ec
guillaume4ssig <gu.dev@laposte.net>
2025-10-13 10:19:13 +0200

    probleme de push

--- TD1/fib/fib.c
+++ TD1/fib/fib.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#includes <time.h>
+#include "fib.h"
+#include <stdlib.h>
 
 int FibonacciNaive(int n){
     if(n==0||n==1){
@@ -8,17 +8,17 @@ int FibonacciNaive(int n){
         return FibonacciNaive(n-1)+FibonacciNaive(n-2);}
 }
 
-int Fibonacci(int n){
+int FibonacciDyn(int n){
+    if (n <= 1)
+        return n;
 
-}
+    int DP_Fib[n + 1];  // tableau statique dans la fonction
+    DP_Fib[0] = 0;
+    DP_Fib[1] = 1;
 
+    for (int i = 2; i <= n; i++) {
+        DP_Fib[i] = DP_Fib[i - 1] + DP_Fib[i - 2];
+    }
 
-
-int main(){
-    clock_t start, end;
-    double seconds;
-
-    start = clock();
-
-    printf("%i", FibonacciNaive(10));
+    return DP_Fib[n];
 }
