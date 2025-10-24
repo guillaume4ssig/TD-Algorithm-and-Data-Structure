ffc897473d602768464164ee800a5229cbfe7f96
Guillaume S <gu.dev@laposte.net>
2025-10-24 15:41:46 +0200

    commit oublié mercredi soir, ajout valeurs dans fichiers à part et fonctions de tri complexe créées

--- TD2/ex2/main.c
+++ TD2/ex2/main.c
@@ -8,6 +8,66 @@ int main(void)
 {
     // implement main here
     // create array example using malloc
+
+    int n = 1000;
+    int *tableau = malloc(n * sizeof(int));
+    FILE *f = fopen("../valeurs_unsorted.txt", "r");
+    for (int i = 0; i < n; i++)
+        fscanf(f, "%d", &tableau[i]);
+    fclose(f);
+
     // use sorting algorithms and compare runtime
+    clock_t debut, findebut;
+    double tempsExecution;
+
+    int *copie = malloc(n * sizeof(int));
+
+    // Selection Sort
+    copy_array(tableau, copie, n);
+    debut = clock();
+    selection_sort(copie, n);
+    findebut = clock();
+    tempsExecution = ((double)(findebut - debut)) / CLOCKS_PER_SEC;
+    printf("Selection Sort: %.6f sec, sorted? %s\n", tempsExecution,
+        is_sorted_nondecreasing(copie, n) ? "yes" : "no");
+
+    // Insertion Sort
+    copy_array(tableau, copie, n);
+    debut = clock();
+    insertion_sort(copie, n);
+    findebut = clock();
+    tempsExecution = ((double)(findebut - debut)) / CLOCKS_PER_SEC;
+    printf("Insertion Sort: %.6f sec, sorted? %s\n", tempsExecution,
+        is_sorted_nondecreasing(copie, n) ? "yes" : "no");
+
+    // Bubble Sort
+    copy_array(tableau, copie, n);
+    debut = clock();
+    bubble_sort(copie, n);
+    findebut = clock();
+    tempsExecution = ((double)(findebut - debut)) / CLOCKS_PER_SEC;
+    printf("Bubble Sort: %.6f sec, sorted? %s\n", tempsExecution,
+        is_sorted_nondecreasing(copie, n) ? "yes" : "no");
+
+    // Merge Sort
+    copy_array(tableau, copie, n);
+    debut = clock();
+    merge_sort(copie, n);
+    findebut = clock();
+    tempsExecution = ((double)(findebut - debut)) / CLOCKS_PER_SEC;
+    printf("Merge Sort: %.6f sec, sorted? %s\n", tempsExecution,
+        is_sorted_nondecreasing(copie, n) ? "yes" : "no");
+
+    // Quick Sort
+    copy_array(tableau, copie, n);
+    debut = clock();
+    quick_sort(copie, n);
+    findebut = clock();
+    tempsExecution = ((double)(findebut - debut)) / CLOCKS_PER_SEC;
+    printf("Quick Sort: %.6f sec, sorted? %s\n", tempsExecution,
+        is_sorted_nondecreasing(copie, n) ? "yes" : "no");
+
+    free(tableau);
+    free(copie);
     return 0;
 }
--- TD2/ex2/sort.c
+++ TD2/ex2/sort.c
@@ -5,22 +5,21 @@
 void selection_sort(int *arr, int n)
 {
     // selection sort iterative
-    for (int i = 0; i < n-2; i++){
+    for (int i = 0; i < n-1; i++){
         int min = i;
-        for (int j = i+1; j < n-1; j++){
+        for (int j = i+1; j < n; j++){
             if (arr[min] > arr[j]){
                 min = j;
             }
         }
-        swap_int(arr[min], arr[i]);
+        swap_int(&arr[min], &arr[i]);
     }
-    return arr;
 }
 
 void insertion_sort(int *arr, int n)
 {
     // insertion sort iterative
-    for (int i = 1; i < n-1; i++){
+    for (int i = 1; i < n; i++){
         int key = arr[i];
         int j = i-1;
         while (j >= 0 && arr[j] > key){
@@ -29,7 +28,6 @@ void insertion_sort(int *arr, int n)
         }
         arr[j+1] = key;
     }
-    return arr;
 }
 
 void bubble_sort(int *arr, int n)
@@ -49,11 +47,34 @@ void merge(int *arr, int *tmp, int l, int m, int r)
 {
     // merge function
 
+    int i = l, j = m + 1, k = l;
+    while (i <= m && j <= r){
+        if (arr[i] <= arr[j])
+            tmp[k++] = arr[i++];
+        else
+            tmp[k++] = arr[j++];
+    }
+
+    while (i <= m)
+        tmp[k++] = arr[i++];
+
+    while (j <= r)
+        tmp[k++] = arr[j++];
+
+    for (i = l; i <= r; i++)
+        arr[i] = tmp[i];
 }
 
 void mergesort_rec(int *arr, int *tmp, int l, int r)
 {
     // recursive steps of merge sort
+    if (l >= r)
+        return;
+
+    int m = (l + r) / 2;
+    mergesort_rec(arr, tmp, l, m);
+    mergesort_rec(arr, tmp, m + 1, r);
+    merge(arr, tmp, l, m, r);
 }
 
 void merge_sort(int *arr, int n)
@@ -66,14 +87,41 @@ void merge_sort(int *arr, int n)
     mergesort_rec(arr, tmp, 0, n - 1);
     free(tmp);
 }
+
+
 int partition(int *arr, int l, int r)
 {
     // partition function
+    int pivot = arr[r];
+    int i = l - 1;
+
+    for (int j = l; j < r; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+
+    int tmp = arr[i + 1];
+    arr[i + 1] = arr[r];
+    arr[r] = tmp;
+
+    return i + 1;
 }
 
 void quick_sort_rec(int *arr, int l, int r)
 {
     // recursive steps of quick sort
+    if (l < r)
+    {
+        int p = partition(arr, l, r);
+        quick_sort_rec(arr, l, p - 1);
+        quick_sort_rec(arr, p + 1, r);
+    }
 }
 
 void quick_sort(int *arr, int n)
